537f2f762f59a309766b8dd760e1c323c30efdd1
Jack <skelton.jt9@gmail.com>
2022-02-08 21:52:36 -0500

    initial logic attempt

---
+++ Game/Models/Game.cs
@@ -0,0 +1,46 @@
+// using System;
+
+// namespace Scrabble.Models
+// {
+//   public class Program
+//   {
+//     public int CheckScore(string word){
+//       var split = word.Split(' ');
+//       var count = 0;
+//       for (i = 0; i < split.length; i++){
+//         count ++;
+//       }
+//       return count;
+      // char [] ch =word.ToCharArray(); 
+//     }
+//   }
+// }
+
+//     {
+//         String word = "robot";
+
+//         String[] token = word.Split(); // Something should be placed into the () to 
+//         //print letter by letter??
+
+//         foreach (String r in token)
+//         {
+//             Console.WriteLine(r);
+//         }
+//     }
+// public void WriteLetters(string s)
+// {
+//     foreach (char c in s)
+//     {
+//         Console.WriteLine(c); 
+//     }
+// }
+// If you really want to see it in an explicit character array, do the following:
+
+// string s = "robot";
+// char[] charArray = s.ToCharArray();
+// If you then want to print that out (1 per line), do the following:
+
+// for (int i = 0; i < charArray.Length; i++)
+// {
+//     Console.WriteLine(charArray[i]);
+// }
