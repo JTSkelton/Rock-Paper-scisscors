e97758798790c4e9a3c14505613ec91bbd656240
Jack <skelton.jt9@gmail.com>
2022-02-09 21:37:44 -0500

    Added dictionary
    Co-authored-by: Wajma Niazi <WNiazi@users.noreply.github.com>

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,4 +1,5 @@
  using System;
+ using System.Collections.Generic;
 //  using Scrabble;
 
  public class Program
@@ -7,88 +8,48 @@
     {
       Console.WriteLine("Enter Your Word:");
       var word = Console.ReadLine();
-
-      static int CheckScore(string word)
-      {
-      char[]charArray =word.ToCharArray();
-        var count = 0;
-        //var letters = ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
-      var letters = "aeioulnrst";
-      char[] letter1Array = letters.ToCharArray();
-      var letters2 ="dg";
-      char[] letter2Array = letters2.ToCharArray();
-      var letters3= "bcmp";
-      char[] letter3Array = letters3.ToCharArray();
-      var letters4="fhvwy";
-      char[] letter4Array = letters4.ToCharArray();
-      var letters5="k";
-      char[] letter5Array = letters5.ToCharArray();
-      var letters6="jx";
-      char[] letter6Array = letters6.ToCharArray();
-      var letters7="qz";
-      char[] letter7Array = letters7.ToCharArray();
       
-      foreach (var chars in charArray){
-        foreach(var letter1 in letter1Array){
-          if (chars == letter1) 
-          {
-            count ++;
-          }
-        }
-        foreach(var letter2 in letter2Array){
-          if (chars == letter2)
-          {
-            count += 2;
-          }
-        }
-        
+      
+      static string CheckScore(string word)
+      {
+      char[]charArray = word.ToCharArray();
+      var score = 0;
+      Dictionary<char, int> myDictionary = new Dictionary<char, int>() 
+      { 
+        {'a', 1}, 
+        {'e', 1}, 
+        {'i', 1},
+        {'o', 1},
+        {'u', 1},
+        {'l', 1},
+        {'n', 1},
+        {'r', 1},
+        {'s', 1},
+        {'t', 1},
+        {'d', 2}, 
+        {'g', 2}, 
+        {'b', 3},
+        {'c', 3},
+        {'m', 3},
+        {'p', 3},
+        {'f', 4},
+        {'h', 4},
+        {'v', 4},
+        {'w', 4}, 
+        {'y', 4},
+        {'k', 5},
+        {'j', 8}, 
+        {'x', 8}, 
+        {'q', 10},
+        {'z', 10} 
+      };
+      foreach(var letter in charArray)
+      if(myDictionary.ContainsKey(letter))
+      {
+        score += myDictionary[letter]; 
       }
-      return count;
-    }
-      // var point1 = ("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
-
-      // if(word.Contains("l"))
-      // {
-      // return "yes"; 
-      // } 
-      // else
-      // {
-      // return "no";
-      // }
-    // }
-    Console.WriteLine(CheckScore(word));
+      return "Your score is: " + score.ToString();
+      }
+      System.Console.WriteLine(CheckScore(word));
     }
   }
-
-
-    // public static Dictionary <char, int> ScrabbleDictionary =new Dictionary<char, int> () 
-    //   {
-    //   {"A", 1},  {"E", 1}, {"I",1}, {"O", 1}, {"U", 1}, {"L", 1},  {"N",1}, {"R", 1}, {"S", 1}, {"T", 1}
-    //   };
-
-    // public int GetScore ()
-    // {
-    //     var word =Console.ReadLine();
-    //     char[]charArray =word.ToCharArray();
-    //     int score =0; 
-    //     foreach (KeyValuePair <char, int> ScrabbleDictionary in charArray) 
-    //     {
-    //       if (charArray == scrabble.Value)
-    //       {
-    //       return score ++
-    //       }
-    //     }
-    // }
-
-
-
-// foreach( KeyValuePair<string, string> kvp in openWith )
-// {
-//     Console.WriteLine("Key = {0}, Value = {1}",
-//         kvp.Key, kvp.Value);
-// }
-
-// foreach(KeyValuePair<string, string> entry in myDictionary)
-// {
-//     // do something with entry.Value or entry.Key
-// }
